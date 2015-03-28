a41cfef232e409c63f20de1e18027580e4c11f97
Xavier Via <fernando.via@gmail.com>
2015-03-28 22:12:01 +0100

    Autoresize while keeping hidpi support

--- main.js
+++ main.js
@@ -1,35 +1,60 @@
 'use strict'
 
-let App 		= undefined
-let canvas 	= undefined
+let listeners = {
+	resize: function () {
+		console.log(window.innerWidth)
+		this.canvas.style.width = window.innerWidth + "px"
+		this.canvas.style.height = window.innerHeight + "px"
+		this.canvas.width = window.innerWidth * 2
+		this.canvas.height = window.innerHeight * 2
+	},
+
+	mousemove: function (event) {
+		this.mouse.x = event.offsetX
+		this.mouse.y = event.offsetY
+	}
+}
+
+class App {
+	constructor() {
+		this.mouse = { x: 100, y: 100 }
+		this.start = Date.now()
+		this.canvas = document.querySelector('[id="canvas"]')
+
+		window.addEventListener('resize', listeners.resize.bind(this) )
+		this.canvas.addEventListener('mousemove', listeners.mousemove.bind(this) )
+
+		listeners.resize.call(this)
+
+		this.program = new Program(
+			this.canvas.getContext("webgl"),
+			document.querySelector('[id="script.fragment"]').textContent,
+			document.querySelector('[id="script.vertex"').textContent
+		)
+
+		.addUniform('time', 1, function () {
+			return (Date.now() - this.start) / 1000.0
+		}.bind(this))
+
+		.addUniform('resolution', 2, function () {
+			return [
+				this.canvas.clientWidth * 2,
+				this.canvas.clientHeight * 2
+			]
+		}.bind(this))
+
+		.addUniform('mouse', 2, function () {
+			return [
+				parseFloat(this.mouse.x),
+				parseFloat(this.canvas.clientHeight - this.mouse.y)
+			]
+		}.bind(this))
+
+		.start()
+	}
+}
+
 
 window.addEventListener('DOMContentLoaded', function() {
-	let start 	= Date.now()
-	let mouse 	= {x: 0, y: 0}
-	canvas = document.querySelector('#canvas')
-
-	canvas.addEventListener('mousemove', function (e) {
-		mouse.x = e.offsetX
-		mouse.y = e.offsetY
-	})
-
-	App = new Program(
-		canvas.getContext("webgl"),
-		document.querySelector('#fragment').textContent,
-		document.querySelector('#vertex').textContent
-	)
-
-	.addUniform('time', 1, function () {
-		return (Date.now() - start) / 1000.0
-	})
-
-	.addUniform('resolution', 2, function () {
-		return [canvas.clientWidth * 2, canvas.clientHeight * 2]
-	})
-
-	.addUniform('mouse', 2, function () {
-		return [parseFloat(mouse.x), parseFloat(canvas.clientHeight - mouse.y)]
-	})
-
-	.start()
+	new App
 })
